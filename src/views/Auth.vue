a41c8c2ea3bdcf5ba8aa3a54dbca148973457eeb
Juan Carlos <jcparetas@gmail..com>
2022-03-22 10:43:32 +0100

    first commit

---
+++ src/views/Auth.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <SignIn />
+  </div>
+</template>
+
+<script>
+import SignIn from "../components/SignIn";
+
+export default {
+  components: {
+    SignIn
+  }
+};
+</script>
